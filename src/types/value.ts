1223a8b2a45352a9231dae5440e61528b8de6cff
Christoph Guttandin <chrisguttandin@media-codings.com>
2018-04-17 22:40:15 +0200

    feat: allow regular expressions as values

--- src/types/value.ts
+++ src/types/value.ts
@@ -2,4 +2,4 @@ import { IValueArray, IValueMap } from '../interfaces';
 import { TTransferable } from './transferable';
 import { TTypedArray } from './typed-array';
 
-export type TValue = boolean | null | number | string | TTypedArray | TTransferable | IValueArray | IValueMap;
+export type TValue = boolean | null | number | string | RegExp | TTypedArray | TTransferable | IValueArray | IValueMap;

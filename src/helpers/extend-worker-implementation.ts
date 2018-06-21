2c1f495be839e90585833bb0b1480bc09dca1e4a
Christoph Guttandin <chrisguttandin@media-codings.com>
2018-06-21 18:36:48 +0200

    style: apply linting rules

--- src/helpers/extend-worker-implementation.ts
+++ src/helpers/extend-worker-implementation.ts
@@ -9,8 +9,9 @@ export const extendWorkerImplementation = <T extends IWorkerDefinition>(
     createWorker: (receiver: IReceiver, workerImplementation: TWorkerImplementation<T>) => TDestroyWorkerFunction,
     partialWorkerImplementation: TWorkerImplementation<T>
 ): TWorkerImplementation<T & IDefaultWorkerDefinition> => {
-    // @todo The spread operator can't be used here because TypeScript does not believe that partialWorkerImplementation is an object.
-    return Object.assign({ }, partialWorkerImplementation, <TWorkerImplementation<IDefaultWorkerDefinition>> {
+    return <TWorkerImplementation<T & IDefaultWorkerDefinition>> {
+        // @todo TypeScript does not believe that partialWorkerImplementation is an object.
+        ...(partialWorkerImplementation as object),
         connect: ({ port }) => {
             port.start();
 
@@ -36,5 +37,5 @@ export const extendWorkerImplementation = <T extends IWorkerDefinition>(
 
             return { result: null };
         }
-    });
+    };
 };

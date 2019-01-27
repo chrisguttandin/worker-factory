177fd3908218402289319ca9a788014514502319
Christoph Guttandin <chrisguttandin@media-codings.com>
2019-01-27 21:58:19 +0100

    build(config): update grunt config

--- config/grunt/sh.js
+++ config/grunt/sh.js
@@ -1,21 +1,17 @@
-module.exports = (grunt) => {
-    const continuous = (grunt.option('continuous') === true);
-
-    return {
-        'build-es2018': {
-            cmd: 'tsc -p src/tsconfig.json'
-        },
-        'build-es5': {
-            cmd: 'rollup -c config/rollup/bundle.js'
-        },
-        'lint': {
-            cmd: 'tslint --config config/tslint/src.json --project src/tsconfig.json src/*.ts src/**/*.ts'
-        },
-        'test-integration': {
-            cmd: `karma start config/karma/config-integration.js ${ continuous ? '--concurrency Infinity' : '--single-run' }`
-        },
-        'test-unit': {
-            cmd: `karma start config/karma/config-unit.js ${ continuous ? '--concurrency Infinity' : '--single-run' }`
-        }
-    };
+module.exports = {
+    'build-es2018': {
+        cmd: 'tsc -p src/tsconfig.json'
+    },
+    'build-es5': {
+        cmd: 'rollup -c config/rollup/bundle.js'
+    },
+    'lint': {
+        cmd: 'tslint --config config/tslint/src.json --project src/tsconfig.json src/*.ts src/**/*.ts'
+    },
+    'test-integration': {
+        cmd: 'karma start config/karma/config-integration.js --single-run'
+    },
+    'test-unit': {
+        cmd: 'karma start config/karma/config-unit.js --single-run'
+    }
 };

a3a97ad27f6bf4809670d7a8d996192d1beac9b1
Christoph Guttandin <chrisguttandin@media-codings.com>
2019-08-21 21:11:01 +0200

    build(config): update linting tasks

--- config/grunt/sh.js
+++ config/grunt/sh.js
@@ -1,17 +1,27 @@
-module.exports = {
-    'build-es2018': {
-        cmd: 'tsc -p src/tsconfig.json'
-    },
-    'build-es5': {
-        cmd: 'rollup -c config/rollup/bundle.js'
-    },
-    'lint': {
-        cmd: 'tslint --config config/tslint/src.json --project src/tsconfig.json src/*.ts src/**/*.ts'
-    },
-    'test-integration': {
-        cmd: 'karma start config/karma/config-integration.js --single-run'
-    },
-    'test-unit': {
-        cmd: 'karma start config/karma/config-unit.js --single-run'
-    }
+module.exports = (grunt) => {
+    const fix = (grunt.option('fix') === true);
+
+    return {
+        'build-es2018': {
+            cmd: 'tsc -p src/tsconfig.json'
+        },
+        'build-es5': {
+            cmd: 'rollup -c config/rollup/bundle.js'
+        },
+        'lint-config': {
+            cmd: `eslint --config config/eslint/config.json ${ (fix) ? '--fix ' : '' }--report-unused-disable-directives *.js config/**/*.js`
+        },
+        'lint-src': {
+            cmd: 'tslint --config config/tslint/src.json --project src/tsconfig.json src/*.ts src/**/*.ts'
+        },
+        'lint-test': {
+            cmd: `eslint --config config/eslint/test.json ${ (fix) ? '--fix ' : '' }--report-unused-disable-directives test/**/*.js`
+        },
+        'test-integration': {
+            cmd: 'karma start config/karma/config-integration.js --single-run'
+        },
+        'test-unit': {
+            cmd: 'karma start config/karma/config-unit.js --single-run'
+        }
+    };
 };
